478dc6b9e12d443ce3063e84737f26754987a83d
Victor Bursztyn <victorbursztyn@Victors-MacBook-Pro.local>
2015-05-16 13:21:01 -0300

    Removing accidental push of venv.

--- venv/bin/train_chunker.py
+++
@@ -1,6 +0,0 @@
-#!/Users/victorbursztyn/Documents/PESC/BRI/FinalTask/DESHIN/venv/bin/python2.7
-# EASY-INSTALL-DEV-SCRIPT: 'nltk-trainer==0.9','train_chunker.py'
-__requires__ = 'nltk-trainer==0.9'
-__import__('pkg_resources').require('nltk-trainer==0.9')
-__file__ = '/Users/victorbursztyn/Documents/PESC/BRI/FinalTask/DESHIN/venv/src/nltk-trainer/train_chunker.py'
-exec(compile(open(__file__).read(), __file__, 'exec'))
