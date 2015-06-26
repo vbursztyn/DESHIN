faee78c85069f632ea2438e8d0570de7ff71270f
Victor Bursztyn <victorbursztyn@Victors-MacBook-Pro.local>
2015-06-26 20:02:16 -0300

    Fixes #2

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -11,17 +11,30 @@ class Evaluator():
 		self.idealSummary = idealSummary
 	
 
-	def run(self):
-		# Combinates precision, recall or whatever other evaluation measure, into one final test result.
-		# At a first glance, F1 offers a fair combination.
-		if not self.summary or not self.idealSummary:
-			raise Exception("Evaluator misscalled - set it first")
-		
+	def calculatePrecision(self):
 		cooccurrences = 0
 		for sentence in self.summary:
 			if sentence in self.idealSummary:
 				cooccurrences = cooccurrences + 1
 
 		precision = cooccurrences / float( len(self.summary) )
-		print "Precision is: " + str(precision)
+
 		return precision
+
+
+	def run(self):
+		# Picked and calculated F1, mostly based on:
+		# F1: http://www.isi.edu/natural-language/projects/webclopedia/pubs/02hlt-neats.pdf
+		# Precision and Recall: http://www.aclweb.org/anthology/P03-1048
+
+		if not self.summary or not self.idealSummary:
+			raise Exception("Evaluator misscalled - set it first")
+		
+		precision = self.calculatePrecision()
+		# Sizes (in sentences) are equal, thus precision is numerically equal to recall.
+		# And if: F1 = (2 * precision * recall) / (precision + recall)
+		# Then F1 is also numerically equal to precision.
+
+		print "F1 is: " + str(precision)
+		return precision
+
